8cf3926147ed01c94757844c3aca020752b9fec5
XOT <54303621+minexo79@users.noreply.github.com>
2020-02-11 14:21:36 +0800

    fixed

--- js/info.js
+++ js/info.js
@@ -1,3 +1,3 @@
 function about() {
-    alert('版本：20200211\n此為Viva! VTuber 支援的四位VTuber（璐洛洛、蝦拉拉）的Twitch直播整合台，方便大家觀看直播。');
+    alert('版本：20200211\n此為Viva! VTuber 支援的二位VTuber（璐洛洛、蝦拉拉）的Twitch直播整合台，方便大家觀看直播。');
 }
